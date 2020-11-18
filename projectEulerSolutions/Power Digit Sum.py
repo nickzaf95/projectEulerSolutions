cd94c70a5cfb1a21aa1b2205e218b7619c4a2061
nickzaf95 <nicholaszafiriou@gmail.com>
2020-11-18 10:12:37 +0000

    cleaned up code

--- projectEulerSolutions/Power Digit Sum.py
+++ projectEulerSolutions/Power Digit Sum.py
@@ -1,14 +1,3 @@
-# def power_digit_sum(x, n)
-#     answer = 0
-#     first = x ** n
-#     second = first.to_s.split("")
-#     second.each do |i|
-#         i = i.to_i
-#         answer = answer + i
-#     end
-#     return answer
-# end
-
 def splitString(word):
     return [char for char in word]
 
