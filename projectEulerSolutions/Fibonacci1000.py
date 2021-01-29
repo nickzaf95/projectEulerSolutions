32d2d7bc87fe76062ed9f22ec1571f1458b3be62
nickzaf95 <nicholaszafiriou@gmail.com>
2021-01-29 12:32:54 +0100

    1000 digit fibonacci number

---
+++ projectEulerSolutions/Fibonacci1000.py
@@ -0,0 +1,33 @@
+# The Fibonacci sequence is defined by the recurrence relation:
+
+# Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+# Hence the first 12 terms will be:
+
+# F1 = 1
+# F2 = 1
+# F3 = 2
+# F4 = 3
+# F5 = 5
+# F6 = 8
+# F7 = 13
+# F8 = 21
+# F9 = 34
+# F10 = 55
+# F11 = 89
+# F12 = 144
+# The 12th term, F12, is the first term to contain three digits.
+
+# What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+def fib_next(last, befLast):
+    return last + befLast
+
+def fibonacci_sequence(n):
+    fib = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+    # print(len(str(fib[len(fib) - 1] )))
+    while (len(str(fib[len(fib) - 1])) != n):
+        fib.append(fib_next(fib[len(fib) - 1], fib[len(fib) - 2]))
+    return len(fib) - 1
+
+
+print(fibonacci_sequence(1000))
